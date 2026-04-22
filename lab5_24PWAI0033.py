bf0fd93df43deeb1756115cfc6af881a255523ec
IMAD AHMAD <hamadahmad123ab@gmail.com>
2026-04-22 14:43:58 +0500

    ...

--- lab5_24PWAI0033.py
+++ lab5_24PWAI0033.py
@@ -1,4 +1,5 @@
-for i in range(5):
+i = 0
+while i < 5:
     p = input(f"Enter password: ")
     s = (len(p)>=8) + any(c.isdigit() for c in p) + (p!=p.lower())
     print(f"Strength: {['Very Weak','Weak','Moderate','Strong'][s]}")
@@ -7,4 +8,5 @@ for i in range(5):
         print(f"AI training Level: Beginner → Intermediate")
         break
     print(f"Attempts remaining: {4-i}")
+    i = i + 1
 else: print(f"Final Result: Password is not strong enough.")
